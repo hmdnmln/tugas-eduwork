2ff0ac9875c78542996ec03e9ae3b17a5bf6a1af
Hamdan Maulana <hamdan.maulanaken@gmail.com>
2025-03-26 20:53:01 +0700

    Create conditionalvariable.js

---
+++ cond var-map-filter/conditionalvariable.js
@@ -0,0 +1,10 @@
+const nilai = 50;
+
+const hasil = nilai > 40 ? 'LULUS' : 'TIDAK LULUS';
+console.log(hasil);
+
+const kondisiAND = nilai < 90 && 'LULUS';
+console.log(kondisiAND);
+
+const kondisiOR = nilai > 90 || 'TIDAK LULUS';
+console.log(kondisiOR);
